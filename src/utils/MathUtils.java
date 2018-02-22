fdaed0dd604fd07b6b2235db5107ffe3f04e18a4
cwmilch <cartermilch@gmail.com>
2018-02-22 12:53:13 -0600

    Modify method signature

--- src/utils/MathUtils.java
+++ src/utils/MathUtils.java
@@ -7,16 +7,16 @@ public class MathUtils {
      * Scale a GraphPoint's y-axis location between a given min and max value
      *
      * @param price  the price stored with a GraphPoint
-     * @param min    the min price to scale the GraphPoint between
-     * @param max    the max price to scale the GraphPoint between
+     * @param max    the maximum price to scale the GraphPoint between
+     * @param min    the minimum price to scale the GraphPoint between
      * @param y      the y value to scale
      * @param height the height of the panel the y-value is being scaled on
      * @return the scaled value
      */
-    public static double getScaledY(double price, double min, double max, int y, int height) {
+    public static double getScaledY(double price, double max, double min, int y, int height) {
         int maxY = y + height - PADDING_Y;
         int minY = y + PADDING_Y;
 
-        return (((maxY - minY) * (price - min)) / (max - min)) + minY;
+        return (((maxY - minY) * (price - max)) / (min - max)) + minY;
     }
 }
